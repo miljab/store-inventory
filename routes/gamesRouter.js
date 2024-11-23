d546c60948bdb3654da7c14f63ab5145fefad34b
miljab <jablonskimilosz1337@gmail.com>
2024-11-24 00:36:27 +0100

    Add required data to render gamesList

--- routes/gamesRouter.js
+++ routes/gamesRouter.js
@@ -3,5 +3,6 @@ const gamesRouter = Router();
 const gamesController = require("../controllers/gamesController");
 
 gamesRouter.get("/", gamesController.gamesGet);
+gamesRouter.get("/category/:id", gamesController.categoryGamesGet);
 
 module.exports = gamesRouter;
