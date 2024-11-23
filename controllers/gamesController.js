6b029ebf18f9cfa19df0b2b89beccfeb3883d8cf
miljab <jablonskimilosz1337@gmail.com>
2024-11-24 00:34:05 +0100

    Add list of games with their categories

--- controllers/gamesController.js
+++ controllers/gamesController.js
@@ -1,9 +1,34 @@
 const db = require("../db/queries");
 
-function gamesGet(req, res) {
-  res.render("gameList", { categories: req.categories });
+async function gamesGet(req, res) {
+  const rows = await db.getGamesWithCategories();
+  const gamesMap = new Map();
+
+  rows.forEach((row) => {
+    if (!gamesMap.has(row.id)) {
+      gamesMap.set(row.id, {
+        name: row.name,
+        price: row.price,
+        stock: row.stock,
+        description: row.description,
+        image_url: row.image_url,
+        categories: [],
+      });
+    }
+
+    if (row.category_name)
+      gamesMap.get(row.id).categories.push(row.category_name);
+  });
+
+  res.render("gamesList", {
+    categoriesList: req.categories,
+    gamesList: gamesMap,
+  });
 }
+
+function categoryGamesGet(req, res) {}
 
 module.exports = {
   gamesGet,
+  categoryGamesGet,
 };
