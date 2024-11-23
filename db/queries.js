d2daf4d4949459814f5d1ca5cb9079548bf82a89
miljab <jablonskimilosz1337@gmail.com>
2024-11-24 00:33:08 +0100

    Add query getGamesWithCategories

--- db/queries.js
+++ db/queries.js
@@ -5,6 +5,20 @@ async function getAllCategories() {
   return rows;
 }
 
+async function getAllGames() {
+  const { rows } = await pool.query("SELECT * FROM games");
+  return rows;
+}
+
+async function getGamesWithCategories() {
+  const { rows } = await pool.query(
+    "SELECT Games.*, Categories.name AS category_name FROM Games LEFT JOIN GameCategories ON Games.id = GameCategories.game_id LEFT JOIN Categories ON GameCategories.category_id = Categories.id ORDER BY Games.id"
+  );
+  return rows;
+}
+
 module.exports = {
   getAllCategories,
+  getAllGames,
+  getGamesWithCategories,
 };
