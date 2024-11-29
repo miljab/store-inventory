e39cf2767d7b6a968f708cd4a8bd73ab5a24dd8b
miljab <jablonskimilosz1337@gmail.com>
2024-11-29 21:17:47 +0100

    Create newGame view and route

--- controllers/gamesController.js
+++ controllers/gamesController.js
@@ -1,5 +1,6 @@
 const db = require("../db/queries");
 const gamesMap = require("../utils/gamesMap");
+const { body, validationResult } = require("express-validator");
 
 async function gamesGet(req, res) {
   const rows = await db.getGamesWithCategories();
@@ -34,12 +35,79 @@ async function gameInfoGet(req, res) {
 }
 
 function newGameGet(req, res) {
-  res.render("newGame", { categoriesList: req.categories });
+  res.render("newGame", {
+    categoriesList: req.categories,
+    formData: {},
+  });
 }
+
+const validateGame = [
+  body("gameName")
+    .trim()
+    .notEmpty()
+    .withMessage("Game title is required")
+    .isLength({ min: 1, max: 255 })
+    .withMessage("Game title must be between 1 and 255 characters"),
+  body("gamePrice")
+    .trim()
+    .notEmpty()
+    .withMessage("Price is required.")
+    .isFloat({ decimal_digits: "0,2", min: "0.01" })
+    .withMessage("Price must be greater than 0."),
+  body("gameStock")
+    .trim()
+    .notEmpty()
+    .withMessage("Stock is required.")
+    .isInt({ min: "1" })
+    .withMessage("Stock must be greater than 0."),
+  body("gameImg")
+    .optional({ checkFalsy: true })
+    .trim()
+    .isURL({ protocols: ["http", "https"] })
+    .withMessage("Check if image URL is correct.")
+    .isLength({ max: 255 })
+    .withMessage("Image URL cannot be longer than 255 characters."),
+];
+
+newGamePost = [
+  validateGame,
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      console.log(errors.array());
+      return res.status(400).render("newGame", {
+        categoriesList: req.categories,
+        formData: req.body,
+        errors: errors.array(),
+      });
+    }
+
+    const {
+      gameName,
+      gamePrice,
+      gameStock,
+      gameImg,
+      gameDescription,
+      gameCategories,
+    } = req.body;
+
+    console.log({
+      gameName,
+      gamePrice,
+      gameStock,
+      gameImg,
+      gameDescription,
+      gameCategories,
+    });
+
+    res.redirect("/");
+  },
+];
 
 module.exports = {
   gamesGet,
   gamesInCategoryGet,
   gameInfoGet,
   newGameGet,
+  newGamePost,
 };
--- routes/gamesRouter.js
+++ routes/gamesRouter.js
@@ -5,6 +5,7 @@ const gamesController = require("../controllers/gamesController");
 gamesRouter.get("/", gamesController.gamesGet);
 gamesRouter.get("/category/:id", gamesController.gamesInCategoryGet);
 gamesRouter.get("/game/new", gamesController.newGameGet);
+gamesRouter.post("/game/new", gamesController.newGamePost);
 gamesRouter.get("/game/:id", gamesController.gameInfoGet);
 
 module.exports = gamesRouter;
